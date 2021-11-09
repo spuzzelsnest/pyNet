be264e287d5ac366adf3237d843fead86f59257f
Spuzzelsnest <j.mpdesmet@gmail.com>
2021-11-09 11:07:02 +0100

    update gitignore

--- mail.py
+++ mail.py
@@ -1,1 +1,18 @@
+import smtplib
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
  
+server = smtplib.SMTP('smtp.office365.com', 587)
+
+server.ehl0()
+with open('secret.txt', 'r') as f:
+    password =f.read() 
+
+server.log('jan.desmet@sint-lievenshoutem.be', 'password')
+
+msg = MIMEMultipart()
+msg['From'] = 'noreply@sint-lievens-houtem.be'
+msg['To'] = 'helpdesk@sint-lievens-houtem.be'
+msg['Subject'] = 'pyNet - Update'
