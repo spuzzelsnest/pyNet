ecd101596acad43dfad3a4060ddb1e5bd3ea04cc
Spuzzelsnest <j.mpdesmet@gmail.com>
2021-11-09 11:09:26 +0100

    update users

--- mail.py
+++ mail.py
@@ -4,15 +4,15 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
  
-server = smtplib.SMTP('smtp.office365.com', 587)
+server = smtplib.SMTP('smtp.gmail.com', 587)
 
-server.ehl0()
+server.ehlo()
 with open('secret.txt', 'r') as f:
     password =f.read() 
 
-server.log('jan.desmet@sint-lievenshoutem.be', 'password')
+server.log('j.mpdesmet@gmail.com', 'password')
 
 msg = MIMEMultipart()
-msg['From'] = 'noreply@sint-lievens-houtem.be'
+msg['From'] = 'j.mpdesmet@gmail.com'
 msg['To'] = 'helpdesk@sint-lievens-houtem.be'
 msg['Subject'] = 'pyNet - Update'
