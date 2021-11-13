e509a273e31d97227acc6761164f343e5b360390
spuzzelsnest <j.mpdesmet@protonmail.com>
2021-11-13 18:23:45 +0100

    added local and external ip

--- netConf-example.py
+++ netConf-example.py
@@ -1,10 +1,32 @@
+import getpass
+import requests
+from netifaces import interfaces, ifaddresses, AF_INET
 from jnpr.junos import Device
 
-connection = Device(host='ip', user='user', password='pass')
 
-connection.open()
-showVer =  connection.rpc.get_software_information({'format': 'json'})
-connection.close()
+user = input("username: ")
+psswd = getpass.getpass(prompt='Password: ', stream=None)
 
-print(showVer)
+devices = []
+
+extIP = requests.get('http://ifconfig.io/ip').text
+
+print(f'\nexternal IP: ' + extIP)
+
+print("\nFound the following interface(s)\n")
+
+for ifaceName in interfaces():
+    addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':'No IP addr'}] )]
+    print(ifaceName, ', '.join(addresses))
+
+
+for switch in devices:
+
+    connection = Device(host=switch, user=user, password=psswd)
+
+    connection.open()
+    showVer =  connection.rpc.get_software_information({'format': 'json'})
+    connection.close()
+
+    print(showVer)
 
