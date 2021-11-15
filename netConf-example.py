81cb55488e6f25fc4eab1907b9cebc10c06673a6
spuzzelsnest <j.mpdesmet@protonmail.com>
2021-11-15 11:18:01 +0100

    added selection menu for interface

--- netConf-example.py
+++ netConf-example.py
@@ -1,32 +1,48 @@
-import getpass
-import requests
+import getpass, requests, enquiries, ipaddress
 from netifaces import interfaces, ifaddresses, AF_INET
 from jnpr.junos import Device
 
 
-user = input("username: ")
-psswd = getpass.getpass(prompt='Password: ', stream=None)
+#user = input("username: ")
+#psswd = getpass.getpass(prompt='Password: ', stream=None)
 
+ifList = []
 devices = []
+options = []
 
 extIP = requests.get('http://ifconfig.io/ip').text
-
 print(f'\nexternal IP: ' + extIP)
 
 print("\nFound the following interface(s)\n")
 
 for ifaceName in interfaces():
     addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':'No IP addr'}] )]
+    ifList.append(ifaceName)
     print(ifaceName, ', '.join(addresses))
 
 
-for switch in devices:
+i = len(ifList)
 
-    connection = Device(host=switch, user=user, password=psswd)
+print(f'\nfound {i} interfaces')
 
-    connection.open()
-    showVer =  connection.rpc.get_software_information({'format': 'json'})
-    connection.close()
+for idx, val in enumerate(ifList):
+    options.append(ifList[idx])
+    #print(f'{idx +1}:  {val}')
 
-    print(showVer)
+choice = enquiries.choose('Select an interface', options)
+
+print(choice)    
+
+print(f'{ifaddresses(choice)}')
+
+
+#for switch in devices:
+
+#   connection = Device(host=switch, user=user, password=psswd)
+
+#    connection.open()
+#    showVer =  connection.rpc.get_software_information({'format': 'json'})
+#    connection.close()
+
+#    print(showVer)
 
